cbdcf06cacdcc0fdada452ab5a0a0fdf98254833
Germán Rodríguez <grodri@princeton.edu>
2022-10-03 08:29:30 -0400

    Affed shiny apps

---
+++ survival/frailtyShiny.R
@@ -0,0 +1,45 @@
+# Proportional Hazards with Frailty
+# constant hazard is 2 or 1 with gamma unobserved heterogeneity
+# G. Rodríguez / 5 March 2017
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+# set up data
+
+t = seq(.00, 2.30, .02)
+n = length(t)
+d <- group_by(data.frame(
+  group = c(rep("low",n), rep("high",n)),
+  t = rep(t, 2),
+  h = c(rep(1, n), rep(2, n))
+), group)
+d <- mutate(d, H = cumsum(h)*.02, p = h)
+
+# shiny ui
+
+ui <- fluidPage(
+
+   titlePanel("Proportional Hazards with Frailty"),
+
+      mainPanel(
+        plotOutput("hazards"),
+        sliderInput("var","variance of frailty:", 0, 0.5, value=0, step=.01)
+   )
+)
+
+# shiny server
+
+server <- function(input, output) {
+
+   output$hazards <- renderPlot({
+       mutate(d, p = h/(1 + input$var * H)) %>%
+       ggplot(aes(t, h, color=group)) + geom_line() + geom_line(aes(t, p), size=1.2) +
+        scale_y_continuous(limits=c(0,2.2))
+   })
+}
+
+shinyApp(ui = ui, server = server)
+
+
---
+++ survival/heterogeneityShiny.R
@@ -0,0 +1,39 @@
+# Unobserved Heterogeneity
+# G. Rodríguez / 2 march 2017
+
+library(ggplot2)
+library(dplyr)
+
+usw <- read.table("https://data.princeton.edu/eco572/datasets/usWhiteBlackMales2002.dat")
+names(usw) <- c("age", "lxw", "lxb")
+
+us <- data.frame(
+  race = c(rep("white",nrow(usw)),rep("black",nrow(usw))),
+  age = rep(usw$age + 0.5, 2),
+  lx = c(usw$lxw, usw$lxb)) %>% group_by(race)
+  
+us <- mutate(us, 
+  H=-log(lx/100000), 
+  h = lead(H) - H, 
+  Hm = (H + lead(H))/2,
+  h0 = h) %>% filter(!is.na(h))           
+
+
+ui <- shinyUI(fluidPage(
+  titlePanel("Mortality Crossover"),
+  mainPanel(
+    plotOutput("hazards"),
+    sliderInput("var","variance of frailty:", min=0, max=1.2, step=0.1, value= 0)
+  )
+))
+
+server <- shinyServer(function(input, output) {
+  output$hazards <- renderPlot({
+    sigma2 <- input$var
+    mutate(us, h0 = h * exp(sigma2 * Hm)) %>%
+    ggplot(aes(age, log(h0), color=race)) + geom_line() +
+      scale_y_continuous(limits=c(-10, 5))
+  })
+})
+
+shinyApp(ui = ui, server = server)
